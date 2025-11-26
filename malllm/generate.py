37c4f2c0be4be5810f7a4c1d8ae3fe1d2615c1e8
Nguyen-Dang-Khoa-04072004 <khoa47245@gmail.com>
2025-11-26 11:23:05 +0700

    feat: add test script and test-data , fix import error

---
+++ malllm/generate.py
@@ -0,0 +1,23 @@
+'''
+Function: Generate reponse with a given slice
+Note: This script to test generating response by model with a slice. It is not used in workflow
+'''
+import argparse
+from pathlib import Path
+from model.model_config import ModelConfig
+from model.model import AIModel
+
+
+def main(parser_args):
+    config = ModelConfig.from_json_file(parser_args.config_file)
+    model = AIModel(config)
+    file_path = Path("test-slice.txt")
+    tokens = model.tokenize(file_path)
+    print(model.generate(tokens)) 
+
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser(description="MalLLM Code Analysis Tool")
+    parser.add_argument("--config-file", type=str, default="../config/qwen-coder-0.5b.json", help="File path dẫn đến config file")
+    parser.add_argument("--max-workers", type=int, default=5, help="Số lượng process worker song song tối đa")
+    parser.add_argument("--batch-size", type=int, default=3, help="Số lượng mẫu xử lý trong mỗi batch")
+    main(parser.parse_args())
